4bb8352b628001488335cab7692ffd79faeeba8e
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-09-07 18:42:19 -0300

    fixing getPeiod

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const transactionService = require('../services/transactionService.js');
 
 transactionRouter.post('/', transactionService.create);
 transactionRouter.get('/', transactionService.findAll);
-transactionRouter.get('/', transactionService.getPeriod);
+transactionRouter.get('/:period', transactionService.getPeriod);
 transactionRouter.put('/:id', transactionService.update);
 transactionRouter.delete('/:id', transactionService.remove);
 
