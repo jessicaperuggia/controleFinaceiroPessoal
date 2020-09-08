3357ad0b2cf4055a301e29166ed5b93d49d78db1
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-09-08 15:25:05 -0300

    creating component MonthPeriod

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import MonthPeriod from './components/MonthPeriod';
 
 export default function App() {
   return (
     <div className='container'>
       <h1 className='center'>Controle Finaceito Pessoal</h1>
       <div className='row'>
-
+        <MonthPeriod />
       </div>
     </div>
   );
---
+++ client/src/components/MonthPeriod.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MonthPeriod() {
+    return (
+        <div className='container' >
+
+            <input type="date" />
+            <label>Escolha a data</label>
+        </div>
+    )
+}
